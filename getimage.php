5f90b0acbf64d1ed2655503d40bb329451c3c113
Rob Brazier <rob.brazier@me.com>
2013-05-29 21:40:15 +0100

    First Commit

---
+++ getimage.php
@@ -0,0 +1,34 @@
+<?php
+function getimage($page, $imgurl, $options=array(), $tagorurl=true, $cdn=''){
+  new getimage($page, $imgurl, $options, $tagorurl, $cdn);
+}
+
+class getimage {
+
+  private $height;
+  private $width;
+  private $cdn;
+
+  public function __construct($page, $imgurl, $options=array(), $tagorurl=true) {
+    if(!function_exists('thumb')){
+      die('You must download the <a href="https://github.com/bastianallgeier/kirbycms-extensions/tree/master/plugins/thumb">Thumb</a> Plugin to use this Plugin!');
+    }
+    $this->cdn = @$options['cdn'];
+    $urlarray = explode('/', $imgurl);
+    $filename = array_pop($urlarray);
+    $path = $page->root().'/'.$filename;
+    if(!file_exists($imgurl)){
+      $imgurl = str_replace($filename, 'hqdefault.jpg', $imgurl);
+    }
+    if(!file_exists($path)){
+      file_put_contents($path, file_get_contents($imgurl));
+    }
+    if(isset($this->cdn)){
+      echo str_replace(c::get('url'), $this->cdn, thumb($page->images()->find($filename), $options, false));
+    } else {
+      echo thumb($page->images()->find($filename), $options, $tagorurl);
+    }
+
+  }
+
+}
